2d4d1a8cc7bc22f4cd4182aae294e9f2654a0cf4
Sukharevich Sergey <mr.boss2142bf@gmail.com>
2019-03-02 10:58:52 +0300

    graphic 1

--- JS/script.js
+++ JS/script.js
@@ -1,13 +1,41 @@
-Function line(xl, yl, х2, у2 , n) {
-    /*xl, yl – начало линии, х2, у2 – конец, n – толщина */
-    var clinewidth = " WIDTH=" + n + "HEIGHT=" + n  /* строка для учета толщины */
-    var xstr = "" // строка тегов для записи в HTML-документ 
-    var xstr0 = ' <IMG SRC="point.bmp"' + clinewidth + ' STYLE = "positlon:absolute; '
-    var k = (y2 – yl)/(x2 – xl)	// коэффициент наклона линии
-    var x = xl		// начальное значение координаты х
-    /* Формирование строки, содержащей теги <IMG. . . >: */
-    while (x <= х2) {
-    xstr += xstr9 + 'top:' + (yl + k* (x – xl)) + ': left:1 + x + '
-    x++;
-    // запись в документ
-    }}
+var ctx = document.getElementById("myChart").getContext("2d");
+var myChart = new Chart(ctx, {
+  type: "bar",
+  data: {
+    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+    datasets: [
+      {
+        label: "# of Votes",
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(255, 159, 64, 0.2)"
+        ],
+        borderColor: [
+          "rgba(255,99,132,1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)"
+        ],
+        borderWidth: 1
+      }
+    ]
+  },
+  options: {
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true
+          }
+        }
+      ]
+    }
+  }
+});
